649e3b963ce7be14db86ad196d99be209574db93
mohit <mohit.deshmukh@adapty.com>
2020-07-05 17:07:40 +0530

    Updates

---
+++ precache-manifest.a2b5e5574dd78e3fdcaa6e54df48eabb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55dd246940d5d02434bc26ce80e9b212",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7f5b02b34b1e9ac401be",
+    "url": "/monsters-rolodex/static/css/main.af983bca.chunk.css"
+  },
+  {
+    "revision": "1d6d326f4470ed67fe85",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.46e2921d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5b02b34b1e9ac401be",
+    "url": "/monsters-rolodex/static/js/main.b818a5b8.chunk.js"
+  },
+  {
+    "revision": "aa1aeb4ed7e62e71a38e",
+    "url": "/monsters-rolodex/static/js/runtime-main.2292ee2f.js"
+  }
+]);
